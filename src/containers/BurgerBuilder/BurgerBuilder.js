4e9b073276150fc9c31fed98de69c67a5fda9e09
Martin Mederly <martinmederly@gmail.com>
2020-08-29 13:29:58 +0200

    [128] - Starting the Burger Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import Auxi from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render () {
         return (
             <Auxi>
-                <div>
-                    Burger
-                </div>
+                <Burger/>
                 <div>
                     Build controls
                 </div>
