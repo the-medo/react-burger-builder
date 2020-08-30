774af80739806b5ea8f672e822c5432eea127a09
Martin Mederly <martinmederly@gmail.com>
2020-08-30 14:53:23 +0200

    [181] - Sending a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -92,8 +93,28 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
-    }
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice.toFixed(2),
+            customer: {
+                name: 'Medo',
+                address: {
+                    street: 'Dalimilova 6',
+                    zipcode: '13000',
+                    country: 'Czech republic'
+                },
+                email: 'medo@mail.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch( error => {
+                console.log(error);
+            });
+    };
 
     render () {
         const disabledInfo = {
